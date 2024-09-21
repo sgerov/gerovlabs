ced64ef1dca6134bb11de7ec1e73d1139518e35a
Sava Gerov <sgerov@Savas-MacBook-Pro.local>
2024-09-21 20:57:49 +0800

    fix deployment

--- dev-ai-models/src/App.js
+++ dev-ai-models/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React App
+          <code>React</code> app.
         </p>
       </header>
     </div>
